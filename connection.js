26ebd5f93fe53d85d613a58bfa438395e9cd6c4d
johnnymoon <johnyisa@yahoo.com>
2025-08-03 11:39:17 +0100

    commented out "await client.close();". Hoping that render wont say application eited early again

--- connection.js
+++ connection.js
@@ -27,7 +27,9 @@ async function run() {
 
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+  //  await client.close();
+
+  //
   }
 }
 run().catch(console.dir);
