f1a10653ba14687156d9b509947c5352ef142559
johnnymoon <johnyisa@yahoo.com>
2025-08-03 11:19:39 +0100

    Update connection.js
    had to add &ssl=true into the connection string. hoping render would be able to connect to mongodb from here on

--- connection.js
+++ connection.js
@@ -1,6 +1,9 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://johnyisa55:o9rYjkOBBYAStQx1@cluster0.wt56q3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const uri = "mongodb+srv://johnyisa55:o9rYjkOBBYAStQx1@cluster0.wt56q3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb+srv://johnyisa55:o9rYjkOBBYAStQx1@cluster0.wt56q3k.mongodb.net/?retryWrites=true&w=majority&ssl=true&appName=Cluster0";
+//const uri = "mongodb+srv://johnyisa55:o9rYjkOBBYAStQx1@cluster0.wt56q3k.mongodb.net/?retryWrites=true&w=majority&ssl=true&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
